f3636806dfd1c7b9e969d8777c9bc6c8358ef0e6
Bogdan <bogdan.boyko.2018@gmail.com>
2024-10-03 18:54:19 +0300

    Додав файл lab2.sql

---
+++ lab2.sql
@@ -0,0 +1,9 @@
+SELECT name, population FROM cities WHERE population >= 1000000;
+
+SELECT name, population FROM cities WHERE region IN ('E', 'W') ORDER BY population DESC;
+
+SELECT name, population FROM cities WHERE population > 50000 AND region IN ('S', 'C', 'N');
+
+SELECT name, population FROM cities WHERE population > 150000 AND population <= 350000 AND region IN ('E', 'W', 'N') ORDER BY name LIMIT 20;
+
+SELECT name, population FROM cities WHERE region NOT IN ('E', 'W') ORDER BY population DESC LIMIT 10 OFFSET 10;
